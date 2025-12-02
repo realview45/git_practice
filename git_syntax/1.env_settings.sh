12302092c5dc08a1985016b927ee6a9086c2488f
realview45 <kjink0125@gmail.com>
2025-12-02 16:14:59 +0900

    fix

--- git_syntax/1.env_settings.sh
+++ git_syntax/1.env_settings.sh
@@ -47,3 +47,8 @@ git checkout -b main #main 브랜치로 생성 및 변경
  git add .
  git commit -m "1st commit"
  git push origin main
+
+ # .gitignore파일은 git추적목록에서 제외 대상 나열
+ # 주로, 빌드된 파일 또는 중요암호 등이 담긴 파일을 제외
+ # 만약 이미 git에서 추적되고 있는 파일을 제외하려면 캐시 삭제 필요
+ git rm -r --cached .
